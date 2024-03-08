5d740cd04de8fe1a62d327ce9a075774b950e6a4
Ba9A-tata <yogadzakkii@gmail.com>
2024-03-08 16:53:15 +0700

    membuat  void input

--- 089_Bublesort/089_Bublesort.cpp
+++ 089_Bublesort/089_Bublesort.cpp
@@ -6,4 +6,29 @@
 using namespace std;
 
 int arr[20]; //Deklarasi variable global array a dengan ukuran 20 
-int n;
+int n; // Deklarasi variable global n untuk menyimpan banyak elemen array
+
+void input() { //Prosedur untuk input 
+
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array : "; // output ke layar
+		cin >> n; // input dari pengguna 
+		if (n <= 20) // jika n kurang dari atau sama dengan 20
+			break; // keluar dari loop
+		else { // jika n lebih besar dari 20 
+			cout << "/nArray dapat mempunyai maksimal 20 elemen./n"; // output ke layar 
+
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; 1 < n; i++) { // looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ":"; // output ke layar 
+		cin >> arr[i]; // output dari pengguna
+
+	}
+
+}
